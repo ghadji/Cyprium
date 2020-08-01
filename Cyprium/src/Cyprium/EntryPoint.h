20ecc6abb6a2971356732d8d43550259bc1612de
George Hadjidemetriou <georgehadjidemetriou90@gmail.com>
2020-08-02 00:13:38 +0300

    Added spdlog Logger

--- Cyprium/src/Cyprium/EntryPoint.h
+++ Cyprium/src/Cyprium/EntryPoint.h
@@ -7,6 +7,11 @@ extern Cyprium::Application* Cyprium::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Cyprium::Log::Init();
+	CP_CORE_WARN("Initialized Logger!");
+	int a = 2;
+	CP_INFO("Hello Var={0}!", a);
+
 	auto app = Cyprium::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Cyprium/src/Cyprium/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Cyprium
+{
+	class CYPRIUM_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+
+// Core Log macros
+#define CP_CORE_TRACE(...)    ::Cyprium::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CP_CORE_INFO(...)     ::Cyprium::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CP_CORE_WARN(...)     ::Cyprium::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CP_CORE_ERROR(...)    ::Cyprium::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CP_CORE_FATAL(...)    ::Cyprium::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+												  
+// Client Log macros
+#define CP_TRACE(...)		  ::Cyprium::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CP_INFO(...)		  ::Cyprium::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CP_WARN(...)		  ::Cyprium::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CP_FATAL(...)		  ::Cyprium::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define CP_ERROR(...)		  ::Cyprium::Log::GetClientLogger()->error(__VA_ARGS__)
