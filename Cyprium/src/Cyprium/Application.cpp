0be4c193c7521cd135bfc11e19a78a6a2095ef1f
George Hadjidemetriou <georgehadjidemetriou90@gmail.com>
2020-08-06 16:49:51 +0300

    Added abstraction for Vertex Buffer Layouts

--- Cyprium/src/Cyprium/Application.cpp
+++ Cyprium/src/Cyprium/Application.cpp
@@ -12,6 +12,27 @@ namespace Cyprium
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Cyprium::ShaderDataType::Float:	return GL_FLOAT;
+			case Cyprium::ShaderDataType::Float2:	return GL_FLOAT;
+			case Cyprium::ShaderDataType::Float3:	return GL_FLOAT;
+			case Cyprium::ShaderDataType::Float4:	return GL_FLOAT;
+			case Cyprium::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Cyprium::ShaderDataType::Mat4:		return GL_FLOAT;
+			case Cyprium::ShaderDataType::Int:		return GL_INT;
+			case Cyprium::ShaderDataType::Int2:		return GL_INT;
+			case Cyprium::ShaderDataType::Int3:		return GL_INT;
+			case Cyprium::ShaderDataType::Int4:		return GL_INT;
+			case Cyprium::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		CP_CORE_ASSERT(false, "Unknown ShaderDataType specified!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		CP_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -26,46 +47,71 @@ namespace Cyprium
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f,  0.0f, 0.0f,
-			 0.5f,  0.0f, 0.0f,
-			 0.0f,  1.0f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" },
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
 
-		unsigned int indices[3] = { 
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
+		uint32_t indices[3] = { 
 			0, 1, 2
 		};
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 		std::string vertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
 
 		std::string fragmentSrc = R"(
 			#version 330 core
-			
+
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+			in vec4 v_Color;
+
 			void main()
 			{
-				color = vec4(v_Position + 0.5, 1.0);
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
